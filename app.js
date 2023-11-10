d93d277a7369c17dbaa9d6c24abe929321c24fdf
Fulldroper <40297614+Fulldroper@users.noreply.github.com>
2023-11-10 12:11:03 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,67 +2,56 @@ console.clear();
 window.onload = async () => {
   const parsed = window.location.hash.slice(1).split(":");
   const HostID = parsed[0] || false; // адресса піра до якого потрібно підключитися
-  const peer = new Peer();
+  const peer = new Peer({ 
+    config: {'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] },
+    timeout: 120000 // час в мілісекундах (наприклад, 2 хвилини) 
+  });
   const localVideo = document.getElementById('localVideo');
   const hostURL = document.getElementById('hostURL');
   const myURL = `${window.location.protocol}//${window.location.host}${window.location.pathname}`
 
   if (HostID) {
-    const dataConnection  = peer.connect(HostID)
-    
-    dataConnection.on('open', () => {
-      console.log('Connected to host');
-    });
-    
-    dataConnection.on('data', (data) => {
-      // Отримано дані від хоста (це може бути відео)
-      console.log('Received data:', data);
-      // Обробка отриманих даних
-      const blob = new Blob([data], { type: 'video/webm' });
-      localVideo.src = window.URL.createObjectURL(blob);
-    });
+    peer.on('open', (id) => {
 
-  } else {
-    // Запит на доступ до камери
-    navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function handleSuccess(stream) {
+      // Виклик піра A та отримання відеопотоку
+      console.log(HostID);
+      const call = peer.call(HostID, new MediaStream());
       
-      peer.on('open', function(id) {
-        // hostURL.href = `${myURL}#${id}`
-        // hostURL.innerText = `${myURL}#${id}`
-        // hostURL.onclick = () => (window.open(`${myURL}#${id}`,'name','width=393,height=851'), false)
-        hostURL.onclick = () =>  navigator.clipboard.writeText(`${myURL}#${id}`)
-        
-        console.log('My peer ID is: ' + id);
-      })
+      call.on('stream', (stream) => {
+        console.log('Received remote stream:', stream);
+        console.log('Remote stream state:', stream.active);     
+        localVideo.srcObject = null;
+        localVideo.srcObject = stream;
+      });
 
-      localVideo.srcObject = stream;
-      
-      // Відправляємо стрім через PeerJS
-      peer.on('connection', (dataConnection) => {
-
-        dataConnection.on('open', function () {
-          const chunks = [];
-
-          // Кожен раз, коли отримуємо кадр відео, відправляємо його
-          const mediaRecorder = new MediaRecorder(stream);
-          mediaRecorder.ondataavailable = function (event) {
-            if (event.data.size > 0) {
-              chunks.push(event.data);
-              console.log("sent chunks: ", event.data);
-              dataConnection.send(event.data);
-            }
-          };
-
-          mediaRecorder.start();
-
-        })
-      })
-
+      call.on('close', () => {
+        console.log('Call closed at:', new Date());
+      });;
     })
-    .catch(function (error) {
-      console.error('Error accessing camera:', error);
-    });
+  } else {
+    const stream = await new Promise(function (resolve) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(resolve).catch( e => ( console.error('Error accessing camera', e), resolve(false)))
+    })
+    console.log(stream);
+    if (!stream) return;
+        
+    peer.on('open', function(id) {
+      hostURL.style.visibility = "visible"
+      hostURL.onclick = () =>  navigator.clipboard.writeText(`${myURL}#${id}`)
+      localVideo.srcObject = stream;
+      console.log('My peer ID is: ' + id);
+      
+      peer.on('call', (call) => {
+        console.log("маємо підключення");
+        // Відповідь на дзвінок і передача відеопотоку
+        call.on('stream', () => 1)
+        call.on('close', () => {
+          console.log('Call closed at:', new Date());
+        });
+        console.log(stream);
+        call.answer(stream);
+      });
+    })
   }
 }
 
